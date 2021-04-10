3e46e45f3047e732f47d16e0e6ea913676048bbd
Vincent ABEL-GRIMALT <vincent.abel.grimat@gmail.com>
2021-04-10 19:19:27 +0200

    first syscall test reading stdin writing back on stdout

--- main.d
+++ main.d
@@ -2,16 +2,9 @@
 
 import std.stdio;
 
-
-void show_test(int a)
-{
-	writeln("Hello, World!");
-	writefln("Ceci est un test: %d", a);
-	
-}
-
 void main()
 {
-	int[5][2] f = 3;
-	writeln(f);
+	string line;
+        while ((line = readln()) !is null)
+        write(line);
 }
